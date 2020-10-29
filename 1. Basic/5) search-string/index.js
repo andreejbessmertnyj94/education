4826695ab46c3f23ff216ba37cb04dbff2439ddd
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-29 17:14:10 +0300

    update task 1.5

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,15 +1,18 @@
 function searchString(obj) {
-  let query = '', exceptions = [null, undefined, ''], key;
+  const exceptions = [null, undefined, ''];
+  // return '?' + Object.keys(obj).map((cur) => (!exceptions.includes(obj[cur]) && `${cur}=${obj[cur]}`) || '')
+  //     .filter(i => i !== '').join('&')
+  let query = [];
+  let key;
   for (key in obj) {
-    if (!(exceptions.includes(key)) && !(exceptions.includes(obj[key]))) {
-      query += `${key}=${obj[key]}&`;
+    if (!(exceptions.includes(obj[key]))) {
+      query.push(`${key}=${obj[key]}`);
     }
   }
-  if (query) {
-    query = query.slice(0, -1);
-    return '?' + query
+  if (query.length > 0) {
+    return '?' + query.join('&')
   }
-  return query
+  return ''
 }
 
 window.searchString = searchString;
