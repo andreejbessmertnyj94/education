754c3f567e490eef906981285a63c11403ded01b
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-29 16:37:57 +0300

    task 1.5

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,5 +1,15 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+  let query = '', exceptions = [null, undefined, ''], key;
+  for (key in obj) {
+    if (!(exceptions.includes(key)) && !(exceptions.includes(obj[key]))) {
+      query += `${key}=${obj[key]}&`;
+    }
+  }
+  if (query) {
+    query = query.slice(0, -1);
+    return '?' + query
+  }
+  return query
 }
 
 window.searchString = searchString;
