d9b93373bd5c20a7715dd93881e64b8fd465ad85
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-29 13:15:09 +0300

    task 1.4

--- 1. Basic/4) factorial/index.js
+++ 1. Basic/4) factorial/index.js
@@ -1,5 +1,10 @@
 function factorial(n) {
-  // Напиши свой код здесь
+  if (n === 0) {
+    return 1;
+  } else if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 window.factorial = factorial;
