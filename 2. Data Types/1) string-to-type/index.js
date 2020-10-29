8a76d5981d04782d2d0ee0dcdff0edd01dbef448
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-29 18:03:37 +0300

    task 2.1

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,6 +1,23 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+  switch (str) {
+    case 'null':
+      return null;
+    case 'undefined':
+      return undefined;
+    case 'true':
+      return true;
+    case 'false':
+      return false;
+    case '':
+      return str;
+    default:
+      if (isNaN(Number(str))) {
+        return str;
+      } else {
+        return Number(str);
+      }
+  }
+}
 
 window.stringToType = stringToType;
 
