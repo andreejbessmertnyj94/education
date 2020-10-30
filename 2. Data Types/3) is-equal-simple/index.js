d81ae469a6da95f49ff05808de965a523c7c6717
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-30 16:23:34 +0300

    task 2.3

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,5 +1,22 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+  if (a === null || b === null) {
+    return false;
+  }
+
+  let keysA = Object.keys(a);
+  let keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (let index = 0; index <= keysA.length; index++) {
+    if (keysA[index] !== keysB[index] || a[keysA[index]] !== b[keysB[index]]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 window.isEqual = isEqual;
