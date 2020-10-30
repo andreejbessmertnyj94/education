2858b25d0c8a11cefbf6d9c34ed5b5dc145022ed
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-30 15:15:49 +0300

    task 2.2

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,8 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw "Wrong arguments type!";
+  }
+  return a + b;
 }
 
 window.sumCheck = sumCheck;
