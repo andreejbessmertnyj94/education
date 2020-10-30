1c8e481e62284640a4f22d1ab7fd8afebf8382c1
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-30 17:23:20 +0300

    task 3.2

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,6 +1,14 @@
 function findPair(arr) {
-  // Напиши свой код здесь
-};
+  const len = arr.length;
+  for (let index = 0; index < len - 1; index++) {
+    for (let secondIndex = index + 1; secondIndex < len; secondIndex++) {
+      if (arr[index] === arr[secondIndex]) {
+        return arr[index];
+      }
+    }
+  }
+  return null;
+}
 
 window.findPair = findPair;
 
