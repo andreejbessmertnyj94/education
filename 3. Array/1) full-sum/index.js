7bc01e61f2ae92e91d3d91b5f26b620146c10e47
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-10-30 16:46:36 +0300

    task 3.1

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,13 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+  let sum = 0;
+  let index;
+  for (index in args) {
+    if (typeof args[index] !== "number") {
+      throw true;
+    }
+    sum += args[index];
+  }
+  return sum;
 }
 
 window.fullSum = fullSum;
