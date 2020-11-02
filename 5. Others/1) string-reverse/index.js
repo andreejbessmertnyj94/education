d1733dce01c289c4e16a1d2a537f075edd1c34b9
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-02 11:54:52 +0300

    task 5.1

--- 5. Others/1) string-reverse/index.js
+++ 5. Others/1) string-reverse/index.js
@@ -1,8 +1,12 @@
-/*
-  Задача:
-  Добавить функции метод reverse, который будет переворачивать строку на оборот
-  Примеры:
-  'test'.reverse() === 'tset';
-  'bob'.reverse() === 'bob';
-  'abc123'.reverse() === '321cba';
- */
+function reverseString(str) {
+  if (!str) {
+    str = this;
+  }
+  let result = "";
+  for (let index = str.length - 1; index >= 0; index--) {
+    result += str[index];
+  }
+  return result;
+}
+
+String.prototype.reverse = reverseString;
