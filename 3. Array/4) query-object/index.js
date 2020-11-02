750ed965f3140a939a294092006dd8f719cee558
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-02 10:03:38 +0300

    task 3.4

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,6 +1,18 @@
+import stringToType from "../../2. Data Types/1) string-to-type/index.js";
+
 function queryToObject(query) {
-  // Напиши свой код здесь
-};
+  if (query.startsWith("?")) {
+    query = query.slice(1);
+  }
+  const objects = query.split("&");
+  let result = {};
+  let keyValue;
+  for (const element of objects) {
+    keyValue = element.split("=");
+    result[String(keyValue[0])] = stringToType(keyValue[1]);
+  }
+  return result;
+}
 
 window.queryToObject = queryToObject;
 
