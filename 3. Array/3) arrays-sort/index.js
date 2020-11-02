ead5e6f4d0af468f801f3e073a7a7480b217931d
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-02 09:38:25 +0300

    task 3.3

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,6 +1,24 @@
 function arraysSort(arr) {
-  // Напиши свой код здесь
-};
+  let maxNum;
+  let indexAndMaxElement = [];
+  let result = [];
+  for (let index in arr) {
+    maxNum = -Infinity;
+    for (const element of arr[index]) {
+      if (element > maxNum) {
+        maxNum = element;
+      }
+    }
+    indexAndMaxElement.push([index, maxNum]);
+  }
+  indexAndMaxElement.sort(function (a, b) {
+    return a[1] - b[1];
+  });
+  for (const element of indexAndMaxElement) {
+    result.push(arr[element[0]]);
+  }
+  return result;
+}
 
 window.arraysSort = arraysSort;
 
