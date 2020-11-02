490a3fa4f331dd483da828e13d9b25028ff5a961
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-02 10:12:13 +0300

    task 4.1

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,5 +1,11 @@
-function asyncSum(a, b) {
-  // Напиши свой код здесь
+async function asyncSum(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a === "number" && typeof b === "number") {
+      setTimeout(() => resolve(a + b), 1000);
+    } else {
+      reject("Sorry! Only numbers is allowed.");
+    }
+  });
 }
 
 window.asyncSum = asyncSum;
