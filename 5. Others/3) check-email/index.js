b311f1c4eb1e4d2cd88465560b60ac046c7d7b41
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-02 17:07:06 +0300

    task 5.3

--- 5. Others/3) check-email/index.js
+++ 5. Others/3) check-email/index.js
@@ -1,19 +1,9 @@
-/*
-  Задача
-  Написать функцию которая проверяет, является ли email валидным
-  Прмеры
-  isValidEmail("tima1702@gmail.com") === true
-  isValidEmail("tima1702@gmail") === false
-  isValidEmail("tima1702@gmail.") === false
-  isValidEmail("tima1702gmail.com") === false
-  isValidEmail("example_emai@gmail.com") === true
-  isValidEmail("example8@gmail.com") === true
-  isValidEmail("e2xample8@gmail.com") === true
-  isValidEmail("e2xa.mple8@gmail.com") === true
- */
-
 function isValidEmail(email) {
-  // Напиши свой код здесь
+  if (email.split("@").length !== 2) {
+    return false;
+  }
+  const postfix = email.split("@")[1].split(".");
+  return !!(postfix[0] && postfix[1]);
 }
 
 window.isValidEmail = isValidEmail;
