44f5d3564d48b436e153a86eec08a95a0f84a4af
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-02 11:16:59 +0300

    task 4.2

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,22 @@
-function xhrGet(url) {
-  // Напиши свой код здесь
+async function xhrGet(url) {
+  return new Promise((resolve, reject) => {
+    const oReq = new XMLHttpRequest();
+    oReq.open("GET", url);
+    oReq.setRequestHeader("Content-Type", "application/json");
+    oReq.responseType = "json";
+    oReq.send();
+
+    oReq.onreadystatechange = function () {
+      if (oReq.readyState === XMLHttpRequest.DONE) {
+        const status = oReq.status;
+        if (status === 0 || (status >= 200 && status < 400)) {
+          resolve(oReq.response);
+        } else {
+          reject(oReq.response); // текст ошибки в response
+        }
+      }
+    };
+  });
 }
 
 window.xhrGet = xhrGet;
