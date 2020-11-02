03957d73bee0fc9a738475b54e6fb6e94f676e62
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-02 16:17:17 +0300

    second update task 3.3

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,24 +1,28 @@
 function arraysSort(arr) {
-  let maxNum;
-  let indexAndMaxElement = [];
-  let result = [];
-  for (let index in arr) {
-    // maxNum = -Infinity;
-    // for (const element of arr[index]) {
-    //   if (element > maxNum) {
-    //     maxNum = element;
-    //   }
-    // }
-    maxNum = Math.max(...arr[index]);
-    indexAndMaxElement.push([index, maxNum]);
-  }
-  indexAndMaxElement.sort(function (a, b) {
-    return a[1] - b[1];
+  // let maxNum;
+  // let indexAndMaxElement = [];
+  // let result = [];
+  // for (let index in arr) {
+  //   // maxNum = -Infinity;
+  //   // for (const element of arr[index]) {
+  //   //   if (element > maxNum) {
+  //   //     maxNum = element;
+  //   //   }
+  //   // }
+  //   maxNum = Math.max(...arr[index]);
+  //   indexAndMaxElement.push([index, maxNum]);
+  // }
+  // indexAndMaxElement.sort(function (a, b) {
+  //   return a[1] - b[1];
+  // });
+  // for (const element of indexAndMaxElement) {
+  //   result.push(arr[element[0]]);
+  // }
+  // return result;
+  const newArr = JSON.parse(JSON.stringify(arr));
+  return newArr.sort(function (a, b) {
+    return Math.max(...a) - Math.max(...b);
   });
-  for (const element of indexAndMaxElement) {
-    result.push(arr[element[0]]);
-  }
-  return result;
 }
 
 window.arraysSort = arraysSort;
