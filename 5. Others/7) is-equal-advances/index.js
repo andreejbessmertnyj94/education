dcdaa604d1b13996e1471f4234668588c92b0d9e
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-03 13:20:31 +0300

    task 5.7

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,5 +1,36 @@
 function isEqualAdvances(a, b) {
-  // Напиши свой код здесь
+  if (a === null || b === null) {
+    return false;
+  }
+
+  let keysA = Object.keys(a);
+  let keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (let index = 0; index <= keysA.length; index++) {
+    if (
+      keysA[index] === keysB[index] &&
+      a[keysA[index]] === null &&
+      b[keysB[index]] === null
+    ) {
+      return true;
+    }
+    if (
+      keysA[index] === keysB[index] &&
+      (typeof a[keysA[index]] === "object" ||
+        typeof b[keysB[index]] === "object")
+    ) {
+      return isEqualAdvances(a[keysA[index]], b[keysB[index]]);
+    }
+    if (keysA[index] !== keysB[index] || a[keysA[index]] !== b[keysB[index]]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 window.isEqualAdvances = isEqualAdvances;
